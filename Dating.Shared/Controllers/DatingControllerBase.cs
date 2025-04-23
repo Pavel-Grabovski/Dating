c542834cc67203c18b0c6475bd1975f3334969f7
Home <araa9977@mail.ru>
2025-04-23 07:43:28 +0700

    create DatingControllerBase

---
+++ Dating.Shared/Controllers/DatingControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dating.Shared.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class DatingControllerBase : ControllerBase
+{
+    private IMediator _mediator;
+    protected IMediator Mediator => _mediator ??=
+        HttpContext!.RequestServices.GetService<IMediator>()!;
+
+}
