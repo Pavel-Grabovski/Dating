f6ca7464f261916b23cbbec4f0f54e634ea16891
Home <araa9977@mail.ru>
2025-04-28 23:02:22 +0700

    add BaseEvent

---
+++ Dating.Shared/Event/BaseEvent.cs
@@ -0,0 +1,11 @@
+﻿namespace Dating.Shared.Event;
+
+public abstract class BaseEvent : Message
+{
+    public string EventType { get; set; }
+    public int Version { get; set; }
+    protected BaseEvent(string eventType)
+    {
+        EventType = eventType;
+    }
+}
