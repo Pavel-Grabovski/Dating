3c6a64dd9c4b392c7e94ed6cef67a17b7e4c2686
Home <araa9977@mail.ru>
2025-04-28 19:35:17 +0700

    Delete Profile.API and Profile.DB

--- Dating.Profile.DB.Migrator/Migrations/2025_04_24_17_49_CreateTableUserPreferences.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE "UserPreferences"
-(
-    "UserId" uuid NOT NULL,
-    "Gender" "Gender",
-    "YearBirthFrom" integer NOT NULL CHECK ("YearBirthFrom" > 1900),
-    "YearBirthTo" integer NOT NULL CHECK ("YearBirthTo" > 3000),
-    "SearchRadius" integer NOT NULL CHECK ("SearchRadius" > 0),
-    "HavingChildren" bool
-);
