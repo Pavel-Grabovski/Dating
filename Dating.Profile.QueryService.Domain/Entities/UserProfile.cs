2e98c64592317f3a72ce6253e21a670b088c2631
Home <araa9977@mail.ru>
2025-04-29 23:37:16 +0700

    create UserProfile

---
+++ Dating.Profile.QueryService.Domain/Entities/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿namespace Dating.Profile.QueryService.Domain.Entities;
+
+public class UserProfile
+{
+    public Guid Id { get; set; }
+
+    public required string Name { get; set; }
+    
+    public Gender Gender { get; set; }
+
+    public DateOnly Birthday { get; set; }
+
+    public int SearchRadius { get; set; }
+
+    public bool HaveChildren { get; set; }
+}
---
+++ Dating.Profile.QueryService.Domain/GlobalUsing.cs
@@ -0,0 +1,1 @@
+﻿global using Dating.Profile.QueryService.Domain.Enum;
