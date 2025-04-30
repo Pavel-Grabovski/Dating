128f96a69b8181b281996bde5d7538d060c9b409
Home <araa9977@mail.ru>
2025-04-30 23:42:23 +0700

    PackageReference => FrameworkReference Include="Microsoft.AspNetCore.App" in Dating.Profile.QueryService.Infrastructure

--- Dating.Profile.QueryService.Infrastructure/Data/ApplicationDBContext.cs
+++ Dating.Profile.QueryService.Infrastructure/Data/ApplicationDBContext.cs
@@ -2,26 +2,21 @@
 
 public class ApplicationDBContext : DbContext
 {
-    public DbSet<UserProfile> UserProfiles { get; set; }
+    //public DbSet<UserProfile> UserProfiles { get; set; }
 
     public ApplicationDBContext(DbContextOptions options)
         : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        optionsBuilder.LogTo(Console.WriteLine);
+        //optionsBuilder.LogTo(Console.WriteLine);
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.Entity<UserProfile>(entity =>
-        {
-            entity.HasKey(e => e.Id);
-            entity.Property(e => e.Birthday)
-                  .IsRequired();
-
-            entity.Property(e => e.SearchRadius);
-        });
-
+        //modelBuilder.Entity<UserProfile>(entity =>
+        //{
+        //    entity.HasKey(e => e.Id);
+        //});
     }
 }
