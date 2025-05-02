c8a32af22ff8058e93903fc595099ed515bcab98
Home <araa9977@mail.ru>
2025-05-03 02:38:00 +0700

    create ICommandDispatcher

---
+++ Dating.Shared/Dispatchers/ICommandDispatcher.cs
@@ -0,0 +1,7 @@
+﻿namespace Dating.Shared.Dispatchers;
+public interface ICommandDispatcher
+{
+    public void RegisterHandler<T>(Func<T, Task> handler)
+        where T : BaseCommand;
+    public Task SendCommandAsync(BaseCommand command);
+}
--- Dating.Shared/GlobalUsing.cs
+++ Dating.Shared/GlobalUsing.cs
@@ -1,2 +1,3 @@
-﻿global using Dating.Shared.Messages;
+﻿global using Dating.Shared.Commands;
+global using Dating.Shared.Messages;
 global using MediatR;
