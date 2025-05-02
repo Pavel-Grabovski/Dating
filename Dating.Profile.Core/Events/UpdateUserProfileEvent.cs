a208dcb2836f64f0796556033c3e0515c07cb5a3
Home <araa9977@mail.ru>
2025-05-03 01:19:52 +0700

    create class UpdateUserProfileEvent

---
+++ Dating.Profile.Core/Events/UpdateUserProfileEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Dating.Profile.Core.Events;
+
+public class UpdateUserProfileEvent : BaseEvent
+{
+    public required Guid UserId { get; set; }
+    public string? Name { get; set; }
+    public Gender? Gender { get; set; }
+
+    public DateOnly? Birthday { get; set; }
+
+    public bool? HaveChildren { get; set; }
+
+    public Point? Location { get; set; }
+
+    public DateTime WasOnline { get; set; } = DateTime.UtcNow;
+
+    public UpdateUserProfileEvent() : base(nameof(UpdateUserProfileEvent))
+    {
+    }
+}
