4342c910bd61a090d639431d79b95503f213f946
Home <araa9977@mail.ru>
2025-05-03 01:41:18 +0700

    create class UpdateSearchFiltersEvent

---
+++ Dating.Profile.Core/Events/UpdateSearchFiltersEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Dating.Profile.Core.Events;
+
+public class UpdateSearchFiltersEvent : BaseEvent
+{
+    public required Guid UserId { get; set; }
+
+    public required Gender Gender { get; set; }
+
+    public int YearBirthFrom { get; set; }
+
+    public int YearBirthTo { get; set; }
+
+    public int SearchRadius { get; set; }
+
+    public bool? HaveChildren { get; set; }
+
+    public UpdateSearchFiltersEvent() : base(nameof(UpdateSearchFiltersEvent))
+    {
+    }
+}
