dd23ac07f7dcada06535266319a11a00680752a0
Home <araa9977@mail.ru>
2025-05-03 03:56:38 +0700

    create IEventService

---
+++ Dating.Shared/Services/IEventService.cs
@@ -0,0 +1,16 @@
+﻿namespace Dating.Shared.Services;
+ 
+public interface IEventService
+{
+    public Task<IEnumerable<BaseEvent>> GetEventsAsync(
+        Guid aggregateId,
+        CancellationToken ct
+    );
+
+    public Task SaveEventsAsync(
+        Guid aggregateId,
+        IEnumerable<BaseEvent> events,
+        int expectedVersion,
+        CancellationToken ct
+    );
+}
