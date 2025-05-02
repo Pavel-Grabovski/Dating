310f1497ac59b69b470e969c596d8f0875471456
Home <araa9977@mail.ru>
2025-05-03 03:50:31 +0700

    create EventStorage and IEventStorage

---
+++ Dating.Shared/Events/DAO/EventStorage.cs
@@ -0,0 +1,20 @@
+﻿using Marten;
+
+namespace Dating.Shared.Events.DAO;
+
+public class EventStorage(IDocumentSession documentSession) : IEventStorage
+{
+    public async Task<IEnumerable<EventModel>> FindByAggregateId(Guid id, CancellationToken ct)
+    {
+        return await documentSession.Query<EventModel>()
+            .Where(item => item.AggregateId == id)
+            .OrderBy(item => item.Version)
+            .ToListAsync(ct);
+    }
+
+    public async Task SaveAsync(EventModel eventModel, CancellationToken ct)
+    {
+        documentSession.Store(eventModel);
+        await documentSession.SaveChangesAsync(ct);
+    }
+}
---
+++ Dating.Shared/Events/DAO/IEventStorage.cs
@@ -0,0 +1,7 @@
+﻿namespace Dating.Shared.Events.DAO;
+
+public interface IEventStorage
+{
+    public Task SaveAsync(EventModel eventModel, CancellationToken ct);
+    public Task<IEnumerable<EventModel>> FindByAggregateId(Guid id, CancellationToken ct);
+}
