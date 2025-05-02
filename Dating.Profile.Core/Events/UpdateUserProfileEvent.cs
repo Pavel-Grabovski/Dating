f929ea524abcf460e23d849a18e037b59f03938c
Home <araa9977@mail.ru>
2025-05-03 01:43:00 +0700

    fix UpdateUserProfileEvent prop is required

--- Dating.Profile.Core/Events/UpdateUserProfileEvent.cs
+++ Dating.Profile.Core/Events/UpdateUserProfileEvent.cs
@@ -3,16 +3,19 @@
 public class UpdateUserProfileEvent : BaseEvent
 {
     public required Guid UserId { get; set; }
-    public string? Name { get; set; }
-    public Gender? Gender { get; set; }
 
-    public DateOnly? Birthday { get; set; }
+    public required string Name { get; set; }
 
-    public bool? HaveChildren { get; set; }
+    public required Gender Gender { get; set; }
 
-    public Point? Location { get; set; }
+    public required DateOnly Birthday { get; set; }
+
+    public required bool HaveChildren { get; set; }
+
+    public required Point Location { get; set; }
 
     public DateTime WasOnline { get; set; } = DateTime.UtcNow;
+
 
     public UpdateUserProfileEvent() : base(nameof(UpdateUserProfileEvent))
     {
