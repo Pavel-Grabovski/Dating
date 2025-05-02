cd78a6c0db550e996f76e4323d98f9fca2df8462
Home <araa9977@mail.ru>
2025-05-03 05:13:55 +0700

    create interface IEventHandler<T>

--- Dating.Shared/GlobalUsing.cs
+++ Dating.Shared/GlobalUsing.cs
@@ -1,4 +1,5 @@
-﻿global using Dating.Shared.Commands;
+﻿global using Dating.Shared.Aggregates;
+global using Dating.Shared.Commands;
 global using Dating.Shared.Events;
 global using Dating.Shared.Messages;
 global using MediatR;
---
+++ Dating.Shared/Handlers/IEventHandler.cs
@@ -0,0 +1,8 @@
+﻿using Dating.Shared.Aggregates;
+
+namespace Dating.Shared.Handlers;
+public interface IEventHandler<T>
+{
+    public Task<T> GetAggregateByIdAsync(Guid Id, CancellationToken ct);
+    public Task SaveAsync(BaseAggregate item, CancellationToken ct);
+}
