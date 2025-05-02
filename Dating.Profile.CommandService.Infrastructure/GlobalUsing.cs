582dabaa9e0a92897f1ca38c849de0c3bdbb58a5
Home <araa9977@mail.ru>
2025-05-03 04:24:00 +0700

    create  EventService

--- Dating.Profile.CommandService.Infrastructure/GlobalUsing.cs
+++ Dating.Profile.CommandService.Infrastructure/GlobalUsing.cs
@@ -1,2 +1,6 @@
 ﻿global using Dating.Shared.Commands;
 global using Dating.Shared.Dispatchers;
+global using Dating.Shared.Events;
+global using Dating.Shared.Events.DAO;
+global using Dating.Shared.Exceptions;
+global using Dating.Shared.Services;
