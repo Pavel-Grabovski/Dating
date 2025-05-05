aa7288ee00ac548858913b1d1ec612b36105c912
Home <araa9977@mail.ru>
2025-05-06 03:30:48 +0700

    create AuthController

---
+++ Dating.Authentication.API/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using Dating.Authentication.Application.DTOs;
+global using MediatR;
+global using Microsoft.AspNetCore.Mvc;
+global using Dating.Authentication.Application.Commands;
+global using Dating.Authentication.Application.Queries;
