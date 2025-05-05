21a1530c3d31a24b111db021f6a09f0042a6e052
Home <araa9977@mail.ru>
2025-05-06 03:18:17 +0700

    create UnauthorizedException

---
+++ Dating.Authentication.Application/Exceptions/UnauthorizedException.cs
@@ -0,0 +1,6 @@
+﻿namespace Dating.Authentication.Application.Exceptions;
+
+public class UnauthorizedException : ExceptionBase
+{
+    public override int StatusCode { get; protected set; } = 401;
+}
