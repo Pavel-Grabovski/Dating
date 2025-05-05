a545ba2a9067abac9eacd0c5a2b226e1f8cbbb3e
Home <araa9977@mail.ru>
2025-05-06 04:11:26 +0700

    create  class JWTSecurityService

---
+++ Dating.Authentication.Infrastructure/Services/JWTSecurityService.cs
@@ -0,0 +1,36 @@
+﻿namespace Dating.Authentication.Infrastructure.Services;
+
+public class JWTSecurityService(IConfiguration configuration)
+    : IJWTSecurityService
+{
+    public string CreateToken(User user)
+    {
+        string secretKey = configuration["AuthSettings:SecretKey"]!;
+
+        var claims = new List<Claim>()
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+            new Claim(JwtRegisteredClaimNames.Name, user.UserName!),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email!),
+        };
+
+        SymmetricSecurityKey key = new(Encoding.UTF8.GetBytes(secretKey));
+
+        SigningCredentials creds = new(key, SecurityAlgorithms.HmacSha512Signature);
+
+        var tokenHandler = new JsonWebTokenHandler();
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            SigningCredentials = creds,
+            Subject = new ClaimsIdentity(claims),
+            IssuedAt = DateTime.UtcNow,
+            NotBefore = DateTime.UtcNow.AddMinutes(0),
+            Expires = DateTime.UtcNow.AddMinutes(15),
+        };
+
+        string token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return token;
+    }
+}
