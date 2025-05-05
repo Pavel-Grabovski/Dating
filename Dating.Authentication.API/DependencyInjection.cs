bfe55c72a383515d56009600de4e66a23f683cda
Home <araa9977@mail.ru>
2025-05-06 02:40:23 +0700

    Settings swagger

---
+++ Dating.Authentication.API/DependencyInjection.cs
@@ -0,0 +1,37 @@
+﻿namespace Dating.Authentication.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAPIServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddControllers();
+
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddRouting(options => options.LowercaseUrls = true);
+
+        services.AddOpenApi();
+        return services;
+    }
+
+    public static WebApplication UseAPIServices
+        (this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+        app.UseExceptionHandler(options => { });
+        app.UseHttpsRedirection();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+        return app;
+    }
+}
--- Dating.Authentication.API/Program.cs
+++ Dating.Authentication.API/Program.cs
@@ -7,27 +7,18 @@ public class Program
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-        builder.Services.AddOpenApi();
+        builder.Services
+            .AddAPIServices(builder.Configuration);
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
         }
 
-        app.UseHttpsRedirection();
 
-        app.UseAuthorization();
-
-
-        app.MapControllers();
-
+        app.UseAPIServices();
         app.Run();
     }
 }
