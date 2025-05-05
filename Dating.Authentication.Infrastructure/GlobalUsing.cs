3734094c56dead5d1023b7fb03584f7238b6c2aa
Home <araa9977@mail.ru>
2025-05-06 02:17:42 +0700

    create AuthenticationDbContext

---
+++ Dating.Authentication.Infrastructure/GlobalUsing.cs
@@ -0,0 +1,4 @@
+﻿global using Dating.Authentication.Application.Data;
+global using Dating.Authentication.Domain.Model;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
