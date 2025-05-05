506525e5898b5dd604c7839416d59702f7121b49
Home <araa9977@mail.ru>
2025-05-06 03:10:35 +0700

    create class BadRequestException and ExceptionBase

---
+++ Dating.Shared/Application/Exceptions/BadRequestException.cs
@@ -0,0 +1,13 @@
+﻿namespace Dating.Shared.Application.Exceptions;
+
+public class BadRequestException : ExceptionBase
+{
+    public override int StatusCode { get; protected set; } = 400;
+
+    public BadRequestException(string message) : base(message)
+    {
+    }
+    public BadRequestException()
+    {
+    }
+}
