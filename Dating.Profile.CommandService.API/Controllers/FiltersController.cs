91f67910aef49be034e5fd7ba7689ec12def3940
Home <araa9977@mail.ru>
2025-05-08 07:52:39 +0700

    Create  NotImplemented FiltersController and PremiumController

---
+++ Dating.Profile.CommandService.API/Controllers/FiltersController.cs
@@ -0,0 +1,16 @@
+﻿namespace Dating.Profile.CommandService.API.Controllers;
+
+public class FiltersController : ProfileControllerBase
+{
+    [HttpPost]
+    public async Task<IResult> Create()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPut]
+    public async Task<IResult> Update()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Dating.Profile.CommandService.API/Controllers/PremiumController.cs
@@ -0,0 +1,23 @@
+﻿namespace Dating.Profile.CommandService.API.Controllers;
+
+public class PremiumController : ProfileControllerBase
+{
+    [HttpPost]
+    public async Task<IResult> Active()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<IResult> Prolong()
+    {
+        throw new NotImplementedException();
+    }
+
+    [Obsolete]
+    [HttpPost]
+    public async Task<IResult> Deactive()
+    {
+        throw new NotImplementedException();
+    }
+}
