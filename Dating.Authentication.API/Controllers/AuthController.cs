76bdde13e1b7009e6e8dc0b028544f72bbca1f67
Home <araa9977@mail.ru>
2025-05-08 07:24:02 +0700

    refactoring вынес настройку маршрутизации в Dating.Authentication.API.Controllers

--- Dating.Authentication.API/Controllers/AuthController.cs
+++ Dating.Authentication.API/Controllers/AuthController.cs
@@ -1,15 +1,15 @@
 namespace Dating.Authentication.API.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/[controller]/[action]")]
 public class AuthController : DatingControllerBase
 {
-    [HttpPost("login")]
+    [HttpPost]
     public async Task<IResult> Login(LoginRequestDTO dto, CancellationToken cancellationToken)
     {
         return Results.Ok(await Mediator.Send(new LoginQuery(dto), cancellationToken));
     }
 
-    [HttpPost("register")]
+    [HttpPost]
     public async Task<IResult> Register(RegisterUserRequestDTO dto, CancellationToken cancellationToken)
     {
         return Results.Ok(await Mediator.Send(new RegisterCommand(dto), cancellationToken));
