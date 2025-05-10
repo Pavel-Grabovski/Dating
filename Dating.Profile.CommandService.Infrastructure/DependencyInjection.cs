992fbd1002a4f949de1bea9306653c91590d6329
Home <araa9977@mail.ru>
2025-05-10 10:23:41 +0700

    register EventService + поправил строку подключения к бд PostgreSQLConnection

--- Dating.Profile.CommandService.Infrastructure/DependencyInjection.cs
+++ Dating.Profile.CommandService.Infrastructure/DependencyInjection.cs
@@ -1,13 +1,12 @@
-﻿using Dating.Profile.CommandService.Infrastructure.DAO;
+﻿namespace Dating.Profile.CommandService.Infrastructure;
 
-namespace Dating.Profile.CommandService.Infrastructure;
 public static class DependencyInjection
 {
     public static IServiceCollection AddCommandInfrastructureServices(
         this IServiceCollection services,
         IConfiguration configuration)
     {
-        var connectionString = configuration.GetConnectionString("PgConnection")!;
+        var connectionString = configuration.GetConnectionString("PostgreSQLConnection")!;
         services
             .AddMarten(opt => opt.Connection(connectionString))
             .UseLightweightSessions();
@@ -16,6 +15,8 @@ public static class DependencyInjection
         services.AddScoped<IEventKafkaProducer, EventKafkaProducer>();
 
         services.AddScoped<IEventStorage, EventStorage>();
+
+        services.AddScoped<IEventService, EventService>();
 
         return services;
     }
