d4caba453fcf59bfad258c4fdad1d3203ceb2e3b
Home <araa9977@mail.ru>
2025-05-10 10:22:09 +0700

    create EventService

---
+++ Dating.Profile.CommandService.Application/Services/IEventService.cs
@@ -0,0 +1,8 @@
+﻿using Dating.Shared.Domain.Events;
+
+namespace Dating.Profile.Application.Services;
+
+public interface IEventService
+{
+    public Task SaveEventsAsync(BaseEvent baseEvent,CancellationToken ct);
+}
---
+++ Dating.Profile.CommandService.Infrastructure/Services/EventService.cs
@@ -0,0 +1,25 @@
+﻿namespace Dating.Profile.CommandService.Infrastructure.Services;
+
+public class EventService(
+    IEventStorage eventStorage,
+    IEventKafkaProducer eventKafkaProducer, 
+    IConfiguration configuration) : IEventService
+{
+    public async Task SaveEventsAsync(BaseEvent baseEvent, CancellationToken ct)
+    {
+        var eventModel = new EventModel(
+            Id: Guid.NewGuid().ToString(),
+            CreatedAt: DateTime.Now,
+            EventType: "TEST",
+            EventData: baseEvent);
+
+        await eventStorage.SaveAsync(eventModel, ct);
+
+
+        var topic = configuration.GetValue<string>("Kafka:Topic")!; 
+        await eventKafkaProducer.PublishEventAsync(topic, baseEvent);
+
+
+
+    }
+}
